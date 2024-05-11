78130f2b3a52ac819716dab4ae7d9cce61af673e
Mayank <mayank0real0world@gmail.com>
2024-05-11 23:04:45 +0530

    add : verify user api

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -12,16 +12,3 @@ export const signupSchema = z.object({
     email : z.string().email({message : "Invalid email address"}),
     password : z.string().min(6,{message : "password must be at least 6 characters"})
 })
-   
-
-
-
-
-
-
-
-
-
-
-
-
